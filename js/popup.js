aac478d059ad573de20700bb588861b99c22acda
danielbarnur10 <danielbarnur10@gmail.com>
2021-08-23 19:31:00 +0300

    url dl, added error message

--- js/popup.js
+++ js/popup.js
@@ -12,8 +12,8 @@ console.log("I'm the popup.");
 /**
 * For @PRODCTION unmark those:
 */
-// const url_leadhunt = "http://18.118.199.168/connect-extension/";
-// const url_linkedin = "http://18.118.199.168/create-update-liprofile/";
+// const url_leadhunt = "http://18.118.199.168  /connect-extension/";
+// const url_linkedin = "http://18.118.199.168  /create-update-liprofile/";
 
 /**
  *@DEVELOPMENT
@@ -25,7 +25,7 @@ const url_linkedin = "http://127.0.0.1:8000/create-update-liprofile/";
  * 1.
  *@description popup window to leadhunt
 */
-document.getElementById("popup").addEventListener("click", () => { window.open("http://18.119.98.104", "_blank"); });
+document.getElementById("popup").addEventListener("click", () => { window.open("http://18.118.199.168", "_blank"); });
 
 /**
  * 2.
@@ -91,6 +91,9 @@ document.getElementById("linkedin").addEventListener("click", () => {
             document.getElementById("linkedin_success").innerHTML = `Failure: Try refreshing the page and then click here.. `;
         }
       }
+      else{
+        document.getElementById("linkedin_success").innerHTML = `Failure: Something went wrong.. `;
+      }
     }
   );
 });
@@ -131,6 +134,9 @@ document.getElementById("leadhunt").addEventListener("click", () => {
           document.getElementById(
             "leadhunt_success"
           ).innerHTML = `Failure : Try refreshing the page and then click here.. `;
+      }
+      else{
+        document.getElementById("linkedin_success").innerHTML = `Failure: Something went wrong.. `;
       }
     }
 
