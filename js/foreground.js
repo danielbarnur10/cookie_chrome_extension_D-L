df898862e72102b4f9f6775144d63ddfdc15860b
danielbarnur10 <danielbarnur10@gmail.com>
2021-08-24 13:32:02 +0300

    metadata

--- js/foreground.js
+++ js/foreground.js
@@ -2,8 +2,43 @@
  * This file catches the information from the main page of-
  * linkedin while connected and saves the daya in the local chrome storage.
  */
-if(window.location.href==="https://www.linkedin.com/feed/"){
+if (window.location.href.match("linkedin.com")) {
+    console.log("linkedin")
+    let metaData = ""
+    let array = document.querySelectorAll("code")
+    array.forEach(element => {  
+        if (element.innerHTML.match("picture", "^comment", "^following", )) {
 
+            metaData = element.innerHTML
+        }
+
+    });
+    jsonMetaData = JSON.parse(metaData)
+    //name
+    let firstName = jsonMetaData.included[0].firstName
+    let lastName = jsonMetaData.included[0].lastName
+    let fullName = firstName.concat(" ", lastName)
+    //link
+    let userUrl = "https://www.linkedin.com/in/".concat(jsonMetaData.included[0].publicIdentifier)
+    //image
+    let pictureUrl = jsonMetaData.included[0].picture.rootUrl.concat(jsonMetaData.included[0].picture.artifacts[1].fileIdentifyingUrlPathSegment)
+    while (pictureUrl.match("amp;"))
+        pictureUrl = pictureUrl.replace("amp;", "")
+
+
+    chrome.storage.local.set({
+        name: fullName
+    })
+    chrome.storage.local.set({
+        img: pictureUrl
+    })
+    chrome.storage.local.set({
+        link: userUrl
+    })
+}
+/*
+        if(window.location.href==="https://www.linkedin.com/feed/"){
+            
     //href
     const userHref = document.querySelector(".scaffold-layout__sidebar")
     .querySelector(".ember-view.block").href
@@ -11,6 +46,7 @@ if(window.location.href==="https://www.linkedin.com/feed/"){
      * alt:
      * document.querySelector(".ember-view.block").href
      * */
+/*
     //image
     const userImage = document.querySelector(".feed-identity-module__member-photo.profile-rail-card__member-photo.EntityPhoto-circle-5.lazy-image.ember-view")
     .src
@@ -27,7 +63,7 @@ if(window.location.href==="https://www.linkedin.com/feed/"){
 else{
     
     //name
-   const userHref =  window.location.href
+    const userHref =  window.location.href
     
     //img
     const userImage = document.querySelector(".profile-photo-edit__edit-btn >img").src
@@ -42,9 +78,4 @@ else{
     chrome.storage.local.set( {link:userHref})
 }
 
-
-
-    
-
-
-
+*/
