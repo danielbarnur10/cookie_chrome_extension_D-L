60af2528cd60c9075e917c77c13fc460fbf9c660
danielbarnur10 <danielbarnur10@gmail.com>
2021-08-25 20:05:53 +0300

    messages marked

--- js/foreground.js
+++ js/foreground.js
@@ -17,7 +17,7 @@ if (window.location.href.match("linkedin.com")) {
         }
     });
     metaData.included.forEach(element => {
-        if (JSON.stringify(element).match("firstName", "lastName", "picture","publicIdentifier")) {
+        if (JSON.stringify(element).match("firstName", "lastName", "picture", "publicIdentifier")) {
             // console.log(element)
             includedData = element;
         }
@@ -52,3 +52,23 @@ if (window.location.href.match("linkedin.com")) {
         link: userUrl
     });
 };
+
+// const openMessages = document.querySelector("#msg-overlay").children
+
+// for (const child of openMessages) {
+//     if (child.id.match("ember")) {
+//         for(const element of child.children){
+//                 if (element.className.match("msg-overlay-conversation-bubble__content-wrapper relative display-flex flex-column")){
+//                         for(const msgfind of element.children){
+//                             if(msgfind.className.match("msg-s-message-list-container relative display-flex mtA ember-view")){
+//                                 for(const msg of msgfind.querySelectorAll(" div > ul > li")){
+
+//                                     console.log(msg.innerText)
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//         }
+//     }
+
