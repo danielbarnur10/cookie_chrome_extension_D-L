44383c4d24732c4278f0e6f23d32c9377ab18111
danielbarnur10 <danielbarnur10@gmail.com>
2021-08-27 09:48:34 +0300

    production

--- js/popup.js
+++ js/popup.js
@@ -12,14 +12,14 @@ console.log("I'm the popup.");
 /**
 * For @PRODCTION unmark those:
 */
-// const url_leadhunt = "http://leadhunt.services/connect-extension/";
-// const url_linkedin = "http://leadhunt.services/create-update-liprofile/";
+const url_leadhunt = "http://leadhunt.services/connect-extension/";
+const url_linkedin = "http://leadhunt.services/create-update-liprofile/";
 
 /**
  *@DEVELOPMENT
  */
-const url_leadhunt = "http://127.0.0.1:8000/connect-extension/";
-const url_linkedin = "http://127.0.0.1:8000/create-update-liprofile/";
+// const url_leadhunt = "http://127.0.0.1:8000/connect-extension/";
+// const url_linkedin = "http://127.0.0.1:8000/create-update-liprofile/";
 
 /**
  * 1.
